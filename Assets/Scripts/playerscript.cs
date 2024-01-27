2a39e3401a9787a09f2136c601ea9ade6f4c32f7
00AGR <yeppotatinhas@gmail.com>
2024-01-27 04:29:50 -0300

    pegar arma

--- Assets/Scripts/playerscript.cs
+++ Assets/Scripts/playerscript.cs
@@ -28,34 +28,29 @@ public class playerscript : MonoBehaviour
     private float coyoteTime = 0.2f;
     private float coyoteCounter = 0f;
 
+    //armas
+    [SerializeField]
+    private bool canPickWeapon = false;
+    [SerializeField]
+    private GameObject weapon;
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    public void Move(InputAction.CallbackContext value){
-        moveInput = value.ReadValue<Vector2>();
-    }
-
-    public void Jump(InputAction.CallbackContext value)
-    {
-        if ((jumpsRemaining > 0 || coyoteCounter> 0f) && value.performed)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-            jumpsRemaining--;
-        }
-
-        else if (value.canceled && rb.velocity.y > 0)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
-            jumpsRemaining--;
-            coyoteCounter = 0f;
-        }
-    }
-
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(moveInput.x * speed, rb.velocity.y);
+
+        if(rb.velocity.x > 0f)
+        {
+            transform.localScale = new Vector3(0.7f, transform.localScale.y, transform.localScale.z);
+        }
+        else if(rb.velocity.x < 0f)
+        {
+            transform.localScale = new Vector3(-0.7f, transform.localScale.y, transform.localScale.z);
+        }
     }
 
     private void Update()
@@ -81,10 +76,58 @@ public class playerscript : MonoBehaviour
         return false;
     }
 
-    private void OnDrawGizmos()
+    public void Move(InputAction.CallbackContext value)
+    {
+        moveInput = value.ReadValue<Vector2>();
+    }
+
+    public void Jump(InputAction.CallbackContext value)
+    {
+        if ((jumpsRemaining > 0 || coyoteCounter > 0f) && value.performed)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpsRemaining--;
+        }
+
+        else if (value.canceled && rb.velocity.y > 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+            jumpsRemaining--;
+            coyoteCounter = 0f;
+        }
+    }
+
+    public void Weapon(InputAction.CallbackContext value)
+    {
+        if (canPickWeapon)
+        {
+            weapon.transform.SetParent(gameObject.transform);
+            weapon.transform.localPosition = new Vector3(0.7f, 0f, weapon.transform.localPosition.z);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("weapon"))
+        {
+            canPickWeapon = true;
+            weapon = collision.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("weapon"))
+        {
+            canPickWeapon = false;
+            weapon = null;
+        }
+    }
+
+    /*private void OnDrawGizmos()
     {
         Gizmos.color = Color.magenta;
         Gizmos.DrawWireCube(feet.position, feetSize);
-    }
+    }*/
 
 }
