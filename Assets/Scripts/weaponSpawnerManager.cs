6ddde1b79e080455b3b74a38b16ab8fb4f32f52e
00AGR <yeppotatinhas@gmail.com>
2024-01-27 07:00:58 -0300

    spawner att

--- Assets/Scripts/weaponSpawnerManager.cs
+++ Assets/Scripts/weaponSpawnerManager.cs
@@ -11,18 +11,44 @@ public class weaponSpawnerManager : MonoBehaviour
     private int index;
     [SerializeField]
     private float maxTime, minTime;
-
+    [SerializeField]
+    private List <GameObject> weaponsList = new List<GameObject>();
+    [SerializeField]
+    private bool canSpawn = true;
 
     private void Start()
     {
         StartCoroutine(SpawnWeapon());
     }
 
+    private void Update()
+    {
+        for (int i = 0; i < weaponsList.Count; i++)
+        {
+            if (weaponsList[i] == null)
+            {
+                weaponsList.Remove(weaponsList[i]);
+            }
+        }
+    }
+
     IEnumerator SpawnWeapon()
     {
         index = Random.Range(0, spawnPos.Length);
+        canSpawn = true;
 
-        Instantiate(prefab, new Vector3 (spawnPos[index], 7f, 0f), Quaternion.identity);
+        for (int i = 0; i < weaponsList.Count; i++)
+        {
+            if (weaponsList[i] != null && weaponsList[i].transform.position.x == spawnPos[index])
+            {
+                canSpawn = false;
+            }
+        }
+
+        if (canSpawn)
+        {
+            weaponsList.Add(Instantiate(prefab, new Vector3(spawnPos[index], 7f, 0f), Quaternion.identity));
+        }
 
         yield return new WaitForSeconds(Random.Range(minTime, maxTime));
 
