ed87ba5cfba041664b3766fc8f01192271298f36
00AGR <yeppotatinhas@gmail.com>
2024-01-28 03:27:48 -0300

    votos com timer

--- Assets/Scripts/questionsManager.cs
+++ Assets/Scripts/questionsManager.cs
@@ -13,6 +13,11 @@ public class questionsManager : MonoBehaviour
     [SerializeField]
     private float counter = 0;
 
+    private float timer = 10f;
+    public float timerCounter;
+
+    public Image clock;
+
     public Image[] p1Icons;
     public Image[] p2Icons;
 
@@ -41,8 +46,23 @@ public class questionsManager : MonoBehaviour
         if(counter < 0)
         {
             counter = maxTime;
+            timerCounter = timer;
             Time.timeScale = 0;
             questionsOBJ.SetActive(true);
+        }
+
+        if(Time.timeScale == 0)
+        {
+            timerCounter -= Time.unscaledDeltaTime;
+            clock.fillAmount = timerCounter / timer;
+        }
+
+        if(timerCounter < 0f)
+        {
+            p1Choice = Random.Range(0, 2);
+            p2Choice = p1Choice;
+            p1Chose = true;
+            p2Chose = true;
         }
 
         if(p1Chose && p2Chose)
